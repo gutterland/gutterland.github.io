fc6773389420f9c09bc8822e28e4920a20f796d0
gutterland <79710257+gutterland@users.noreply.github.com>
2021-03-02 13:47:23 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,5 @@
 function okBuddy() {
   var rotator = document.getElementById("rotator");
   var result = Math.floor(Math.random() * 32) + 1;
-    rotator.setAttribute(
-    "src",
-    `gutters/guttergacha.gif`
-  );
-  setTimeout(rotator.setAttribute("src",`gutters/gutter${result}.gif`), 2000);
+  rotator.setAttribute("src",`gutters/gutter${result}.gif`);
 };
